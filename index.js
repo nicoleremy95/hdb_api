9c4f1d463273773363a7ac2dcd26fed2e5720cc2
Nicole Remy <nicoleremy95@gmail.com>
2020-10-28 12:25:39 -0700

    created basic server with express and sequelize

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+var allRoutes = require('./controllers');
+
+var db = require('/models');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use('/', allRoutes);
+
+db.sequelize.sync({force:true}).then(function(){
+    app.use.isten(PORT, function(){
+        console.log('App listening on PORT', + PORT)
+    })
+});
